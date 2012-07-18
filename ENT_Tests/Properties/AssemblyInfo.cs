8c1c069e1ed0895004ef63ee8940dc5c20108525
Bridgit <NeNGraphy@gmail.com>
2012-07-18 14:26:13 +0400

    clean up

--- ENT_Tests/Properties/AssemblyInfo.cs
+++ ENT_Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ENT_Client")]
+[assembly: AssemblyTitle("ENT_Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ENT_Client")]
+[assembly: AssemblyProduct("ENT_Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5022f135-42b1-4c36-a13e-c094bb9f645a")]
+[assembly: Guid("bafce6bd-b4ee-494a-aa48-b750e9483d16")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("ENT_Tests")]
--- README.md
+++ README.md
@@ -3,4 +3,7 @@ ENT
 
 C# SQL WCF Entity Framework Linq Client Server
 
-![source-code](https://raw.github.com/Nensha/nensha.github.com/master/img/th1.png)
+TODO and Alternative Solutions
+==============================
+
+- http://msdn.microsoft.com/en-us/data/bb931106.aspx
