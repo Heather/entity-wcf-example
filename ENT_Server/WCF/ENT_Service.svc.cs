31a495866e60ca199f362264cdaf0279e6c47acf
Bridgit <NeNGraphy@gmail.com>
2012-07-18 15:36:19 +0400

    Ok just switch from client to full version of .net to use WCF Data Service

---
+++ ENT_Server/WCF/ENT_Service.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Services;
+using System.Data.Services.Common;
+
+using ENT_Server.Entity;
+
+namespace ENT_Server.WCF {
+    public class WcfDataService1 : DataService<ENTEntities> {
+        public static void InitializeService(DataServiceConfiguration config) {
+            config.SetEntitySetAccessRule("Good", EntitySetRights.AllRead
+                 | EntitySetRights.WriteMerge
+                 | EntitySetRights.WriteReplace);
+            config.SetEntitySetAccessRule("Shop", EntitySetRights.AllRead
+                | EntitySetRights.AllWrite);
+            config.SetEntitySetAccessRule("Cash", EntitySetRights.AllRead);
+
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+            }
+        }
+    }
--- README.md
+++ README.md
@@ -2,8 +2,3 @@ ENT
 ===
 
 C# SQL WCF Entity Framework Linq Client Server
-
-TODO and Alternative Solutions
-==============================
-
-- http://msdn.microsoft.com/en-us/data/bb931106.aspx
