cfb6645e3d5afb7322be50a296177773b0bbb8b8
Bridgit <NeNGraphy@gmail.com>
2012-07-18 08:44:11 +0400

    basic WCF interface

--- ENT_Server/Program.cs
+++ ENT_Server/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using ENT_Server.WCF;
+
 namespace ENT_Server {
     class Program {
         static void Main(string[] args) {
+            }
         }
     }
-}
---
+++ ENT_Server/WCF/WCF_base.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace ENT_Server.WCF {
+
+    [ServiceContract]
+    public interface IENT_Service {
+        [OperationContract]
+        string SayHello(string name);
+        }
+
+    public class ENT_Service : IENT_Service {
+        public string SayHello(string name) {
+            return string.Format("Hello, {0}", name);
+            }
+        }
+
+    class WCF_base {
+        }
+
+    }
